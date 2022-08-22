00a6c6b523cc7e13b8e6c9803e2b82da14c54c4b
Juan Gutiérrez <juanesguti19@gmail.com>
2022-08-22 17:19:16 -0500

    First session - Intro

---
+++ sitio-web/index.js
@@ -0,0 +1,20 @@
+var name = "Juan";
+let nameLet = "Juan let";
+const nameConst = "Juan const";
+
+const age = 45;
+
+const obj = {
+    name: "Juan",
+    age: 23
+};
+
+const vector = [0, 1, 2, 3];
+
+const fun = () => {
+    alert('edad ' + age);
+}
+
+function hello() {
+    alert('Hola ' + name)
+}
